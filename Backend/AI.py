6b47004e02f1ca3e5a3871228fab9be857b655fe
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2023-04-02 10:28:23 +0200

    Funkcje przeniesione na gore

--- Backend/AI.py
+++ Backend/AI.py
@@ -3,6 +3,7 @@ import shutil
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
 from sklearn.metrics import confusion_matrix, classification_report
 from keras.applications import VGG19
 from keras.preprocessing.image import ImageDataGenerator
@@ -19,16 +20,30 @@ def plot_hist(history):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=hist['epoch'], y=hist['accuracy'], name='accuracy', mode='markers+lines'))
     fig.add_trace(go.Scatter(x=hist['epoch'], y=hist['val_accuracy'], name='val_accuracy', mode='markers+lines'))
-    fig.update_layout(width=1000, height=500, title='Accuracy vs. Val Accuracy', xaxis_title='Epoki', yaxis_title='Accuracy', yaxis_type='log')
+    fig.update_layout(width=1000, height=500, title='Accuracy vs. Val Accuracy', xaxis_title='Epoki',
+                      yaxis_title='Accuracy', yaxis_type='log')
     fig.show()
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=hist['epoch'], y=hist['loss'], name='loss', mode='markers+lines'))
     fig.add_trace(go.Scatter(x=hist['epoch'], y=hist['val_loss'], name='val_loss', mode='markers+lines'))
-    fig.update_layout(width=1000, height=500, title='Loss vs. Val Loss', xaxis_title='Epoki', yaxis_title='Loss', yaxis_type='log')
+    fig.update_layout(width=1000, height=500, title='Loss vs. Val Loss', xaxis_title='Epoki', yaxis_title='Loss',
+                      yaxis_type='log')
     fig.show()
 
+def print_layers(model):
+    for layer in model.layers:
+        print(f'layer_name: {layer.name:13} trainable: {layer.trainable}')
 
+def plot_confusion_matrix(cm):
+    # Mulitclass classification, 3 classes
+    cm = cm[::-1]
+    cm = pd.DataFrame(cm, columns=classes, index=classes[::-1])
+
+    fig = ff.create_annotated_heatmap(z=cm.values, x=list(cm.columns), y=list(cm.index), colorscale='ice',
+                                      showscale=True, reversescale=True)
+    fig.update_layout(width=500, height=500, title='Confusion Matrix', font_size=16)
+    fig.show()
 
 
 np.set_printoptions(precision=6, suppress=True)
@@ -46,66 +61,66 @@ data_dir = '../images'
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
 
-train_dir = os.path.join(data_dir, 'train')    # katalog zbioru treningowego
-valid_dir = os.path.join(data_dir, 'valid')    # katalog zbioru walidacyjnego
-test_dir = os.path.join(data_dir, 'test')      # katalog zbioru testowego
+train_dir = os.path.join(data_dir, 'train')  # katalog zbioru treningowego
+valid_dir = os.path.join(data_dir, 'valid')  # katalog zbioru walidacyjnego
+test_dir = os.path.join(data_dir, 'test')  # katalog zbioru testowego
 
-train_Poland_10_dir     =   os.path.join(train_dir,     'Poland_10')
-train_Poland_20_dir     =   os.path.join(train_dir,     'Poland_20')
-train_Poland_50_dir     =   os.path.join(train_dir,     'Poland_50')
-train_Poland_100_dir    =   os.path.join(train_dir,     'Poland_100')
-train_Poland_200_dir    =   os.path.join(train_dir,     'Poland_200')
-train_Poland_500_dir    =   os.path.join(train_dir,     'Poland_500')
+train_Poland_10_dir = os.path.join(train_dir, 'Poland_10')
+train_Poland_20_dir = os.path.join(train_dir, 'Poland_20')
+train_Poland_50_dir = os.path.join(train_dir, 'Poland_50')
+train_Poland_100_dir = os.path.join(train_dir, 'Poland_100')
+train_Poland_200_dir = os.path.join(train_dir, 'Poland_200')
+train_Poland_500_dir = os.path.join(train_dir, 'Poland_500')
 
-valid_Poland_10_dir     =   os.path.join(valid_dir,     'Poland_10')
-valid_Poland_20_dir     =   os.path.join(valid_dir,     'Poland_20')
-valid_Poland_50_dir     =   os.path.join(valid_dir,     'Poland_50')
-valid_Poland_100_dir    =   os.path.join(valid_dir,     'Poland_100')
-valid_Poland_200_dir    =   os.path.join(valid_dir,     'Poland_200')
-valid_Poland_500_dir    =   os.path.join(valid_dir,     'Poland_500')
+valid_Poland_10_dir = os.path.join(valid_dir, 'Poland_10')
+valid_Poland_20_dir = os.path.join(valid_dir, 'Poland_20')
+valid_Poland_50_dir = os.path.join(valid_dir, 'Poland_50')
+valid_Poland_100_dir = os.path.join(valid_dir, 'Poland_100')
+valid_Poland_200_dir = os.path.join(valid_dir, 'Poland_200')
+valid_Poland_500_dir = os.path.join(valid_dir, 'Poland_500')
 
-test_Poland_10_dir      =   os.path.join(test_dir,      'Poland_10')
-test_Poland_20_dir      =   os.path.join(test_dir,      'Poland_20')
-test_Poland_50_dir      =   os.path.join(test_dir,      'Poland_50')
-test_Poland_100_dir     =   os.path.join(test_dir,      'Poland_100')
-test_Poland_200_dir     =   os.path.join(test_dir,      'Poland_200')
-test_Poland_500_dir     =   os.path.join(test_dir,      'Poland_500')
+test_Poland_10_dir = os.path.join(test_dir, 'Poland_10')
+test_Poland_20_dir = os.path.join(test_dir, 'Poland_20')
+test_Poland_50_dir = os.path.join(test_dir, 'Poland_50')
+test_Poland_100_dir = os.path.join(test_dir, 'Poland_100')
+test_Poland_200_dir = os.path.join(test_dir, 'Poland_200')
+test_Poland_500_dir = os.path.join(test_dir, 'Poland_500')
 
 for directory in (train_dir, valid_dir, test_dir):
     if not os.path.exists(directory):
         os.mkdir(directory)
 
 dirs = [
-train_Poland_10_dir ,
-train_Poland_20_dir ,
-train_Poland_50_dir ,
-train_Poland_100_dir,
-train_Poland_200_dir,
-train_Poland_500_dir,
-valid_Poland_10_dir ,
-valid_Poland_20_dir ,
-valid_Poland_50_dir ,
-valid_Poland_100_dir,
-valid_Poland_200_dir,
-valid_Poland_500_dir,
-test_Poland_10_dir  ,
-test_Poland_20_dir  ,
-test_Poland_50_dir  ,
-test_Poland_100_dir ,
-test_Poland_200_dir ,
-test_Poland_500_dir ,
+    train_Poland_10_dir,
+    train_Poland_20_dir,
+    train_Poland_50_dir,
+    train_Poland_100_dir,
+    train_Poland_200_dir,
+    train_Poland_500_dir,
+    valid_Poland_10_dir,
+    valid_Poland_20_dir,
+    valid_Poland_50_dir,
+    valid_Poland_100_dir,
+    valid_Poland_200_dir,
+    valid_Poland_500_dir,
+    test_Poland_10_dir,
+    test_Poland_20_dir,
+    test_Poland_50_dir,
+    test_Poland_100_dir,
+    test_Poland_200_dir,
+    test_Poland_500_dir,
 ]
 
 for dir in dirs:
     if not os.path.exists(dir):
         os.mkdir(dir)
 
-Poland_10_fnames    = os.listdir(os.path.join(base_dir, 'Poland_10'))
-Poland_20_fnames    = os.listdir(os.path.join(base_dir, 'Poland_20'))
-Poland_50_fnames    = os.listdir(os.path.join(base_dir, 'Poland_50'))
-Poland_100_fnames   = os.listdir(os.path.join(base_dir, 'Poland_100'))
-Poland_200_fnames   = os.listdir(os.path.join(base_dir, 'Poland_200'))
-Poland_500_fnames   = os.listdir(os.path.join(base_dir, 'Poland_500'))
+Poland_10_fnames = os.listdir(os.path.join(base_dir, 'Poland_10'))
+Poland_20_fnames = os.listdir(os.path.join(base_dir, 'Poland_20'))
+Poland_50_fnames = os.listdir(os.path.join(base_dir, 'Poland_50'))
+Poland_100_fnames = os.listdir(os.path.join(base_dir, 'Poland_100'))
+Poland_200_fnames = os.listdir(os.path.join(base_dir, 'Poland_200'))
+Poland_500_fnames = os.listdir(os.path.join(base_dir, 'Poland_500'))
 
 Poland_10_fnames = [fname for fname in Poland_10_fnames if fname.split('.')[1].lower() in ['jpg', 'png', 'jpeg']]
 Poland_20_fnames = [fname for fname in Poland_20_fnames if fname.split('.')[1].lower() in ['jpg', 'png', 'jpeg']]
@@ -213,53 +228,53 @@ for i, fname in enumerate(Poland_500_fnames):
         dst = os.path.join(test_Poland_500_dir, fname)
         shutil.copyfile(src, dst)
 
-print('Poland_10 - zbiór treningowy',       len(os.listdir(train_Poland_10_dir  )))
-print('Poland_10 - zbiór walidacyjny',      len(os.listdir(valid_Poland_10_dir  )))
-print('Poland_10 - zbiór testowy',          len(os.listdir(test_Poland_10_dir   )))
+print('Poland_10 - zbiór treningowy', len(os.listdir(train_Poland_10_dir)))
+print('Poland_10 - zbiór walidacyjny', len(os.listdir(valid_Poland_10_dir)))
+print('Poland_10 - zbiór testowy', len(os.listdir(test_Poland_10_dir)))
 
-print('Poland_20 - zbiór treningowy',   len(os.listdir(train_Poland_20_dir)))
-print('Poland_20 - zbiór walidacyjny',  len(os.listdir(valid_Poland_20_dir)))
-print('Poland_20 - zbiór testowy',      len(os.listdir(test_Poland_20_dir)))
+print('Poland_20 - zbiór treningowy', len(os.listdir(train_Poland_20_dir)))
+print('Poland_20 - zbiór walidacyjny', len(os.listdir(valid_Poland_20_dir)))
+print('Poland_20 - zbiór testowy', len(os.listdir(test_Poland_20_dir)))
 
-print('Poland_50 - zbiór treningowy',  len(os.listdir(train_Poland_50_dir)))
+print('Poland_50 - zbiór treningowy', len(os.listdir(train_Poland_50_dir)))
 print('Poland_50 - zbiór walidacyjny', len(os.listdir(valid_Poland_50_dir)))
-print('Poland_50 - zbiór testowy',     len(os.listdir(test_Poland_50_dir)))
+print('Poland_50 - zbiór testowy', len(os.listdir(test_Poland_50_dir)))
 
-print('Poland_100 - zbiór treningowy',  len(os.listdir(train_Poland_100_dir)))
+print('Poland_100 - zbiór treningowy', len(os.listdir(train_Poland_100_dir)))
 print('Poland_100 - zbiór walidacyjny', len(os.listdir(valid_Poland_100_dir)))
-print('Poland_100 - zbiór testowy',     len(os.listdir(test_Poland_100_dir)))
+print('Poland_100 - zbiór testowy', len(os.listdir(test_Poland_100_dir)))
 
-print('Poland_200 - zbiór treningowy',  len(os.listdir(train_Poland_200_dir)))
+print('Poland_200 - zbiór treningowy', len(os.listdir(train_Poland_200_dir)))
 print('Poland_200 - zbiór walidacyjny', len(os.listdir(valid_Poland_200_dir)))
-print('Poland_200 - zbiór testowy',     len(os.listdir(test_Poland_200_dir)))
+print('Poland_200 - zbiór testowy', len(os.listdir(test_Poland_200_dir)))
 
-print('Poland_500 - zbiór treningowy',  len(os.listdir(train_Poland_500_dir)))
+print('Poland_500 - zbiór treningowy', len(os.listdir(train_Poland_500_dir)))
 print('Poland_500 - zbiór walidacyjny', len(os.listdir(valid_Poland_500_dir)))
-print('Poland_500 - zbiór testowy',     len(os.listdir(test_Poland_500_dir)))
+print('Poland_500 - zbiór testowy', len(os.listdir(test_Poland_500_dir)))
 
 train_datagen = ImageDataGenerator(
-    rotation_range=10,      # zakres kąta o który losowo zostanie wykonany obrót obrazów
-    rescale=1./255.,
+    rotation_range=10,  # zakres kąta o który losowo zostanie wykonany obrót obrazów
+    rescale=1. / 255.,
     width_shift_range=0.2,  # pionowe przekształcenia obrazu
-    height_shift_range=0.2, # poziome przekształcenia obrazu
-    shear_range=0.2,        # zares losowego przycianania obrazu
-    zoom_range=0.2,         # zakres losowego przybliżania obrazu
-    horizontal_flip=True,   # losowe odbicie połowy obrazu w płaszczyźnie poziomej
-    fill_mode='nearest'     # strategia wypełniania nowo utworzonych pikseli, któe mogą powstać w wyniku przekształceń
+    height_shift_range=0.2,  # poziome przekształcenia obrazu
+    shear_range=0.2,  # zares losowego przycianania obrazu
+    zoom_range=0.2,  # zakres losowego przybliżania obrazu
+    horizontal_flip=True,  # losowe odbicie połowy obrazu w płaszczyźnie poziomej
+    fill_mode='nearest'  # strategia wypełniania nowo utworzonych pikseli, któe mogą powstać w wyniku przekształceń
 )
 
 # przeskalowujemy wszystkie obrazy o współczynnik 1/255
-valid_datagen = ImageDataGenerator(rescale=1./255.)
+valid_datagen = ImageDataGenerator(rescale=1. / 255.)
 
 train_generator = train_datagen.flow_from_directory(directory=train_dir,
-                                                   target_size=(600, 300),
-                                                   batch_size=1,
-                                                   class_mode='categorical')
+                                                    target_size=(600, 300),
+                                                    batch_size=1,
+                                                    class_mode='categorical')
 
 valid_generator = valid_datagen.flow_from_directory(directory=valid_dir,
-                                                   target_size=(600, 300),
-                                                   batch_size=1,
-                                                   class_mode='categorical')
+                                                    target_size=(600, 300),
+                                                    batch_size=1,
+                                                    class_mode='categorical')
 
 batch_size = 1
 steps_per_epoch = train_size // batch_size
@@ -268,9 +283,9 @@ validation_steps = valid_size // batch_size
 conv_base = VGG19(weights='imagenet', include_top=False, input_shape=(600, 300, 3))
 conv_base.trainable = True
 
-def print_layers(model):
-    for layer in model.layers:
-        print(f'layer_name: {layer.name:13} trainable: {layer.trainable}')
+
+
+
 
 set_trainable = False
 for layer in conv_base.layers:
@@ -292,8 +307,8 @@ model.add(layers.Dense(units=6, activation='softmax'))
 optimizer = optimizers.rmsprop_v2.RMSprop(learning_rate=1e-5)
 
 model.compile(optimizer=optimizer,
-             loss='categorical_crossentropy',
-             metrics=['accuracy'])
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.build((None, 600, 300, 3))
 model.summary()
@@ -306,7 +321,7 @@ history = model.fit(x=train_generator,
 
 plot_hist(history)
 
-test_datagen = ImageDataGenerator(rescale=1./255.)
+test_datagen = ImageDataGenerator(rescale=1. / 255.)
 test_generator = test_datagen.flow_from_directory(
     test_dir,
     target_size=(600, 300),
@@ -317,7 +332,7 @@ test_generator = test_datagen.flow_from_directory(
 
 filenames = test_generator.filenames
 nb_samples = len(filenames)
-y_prob = model.predict(x= test_generator,
+y_prob = model.predict(x=test_generator,
                        steps=nb_samples)
 print(y_prob)
 
@@ -341,16 +356,10 @@ print(classes)
 cm = confusion_matrix(y_true, y_pred)
 cm
 
-def plot_confusion_matrix(cm):
-    # Mulitclass classification, 3 classes
-    cm = cm[::-1]
-    cm = pd.DataFrame(cm, columns=classes, index=classes[::-1])
 
-    fig = ff.create_annotated_heatmap(z=cm.values, x=list(cm.columns), y=list(cm.index), colorscale='ice', showscale=True, reversescale=True)
-    fig.update_layout(width=500, height=500, title='Confusion Matrix', font_size=16)
-    fig.show()
 
-import plotly.figure_factory as ff
+
+
 plot_confusion_matrix(cm)
 
 print(classification_report(y_true, y_pred, target_names=test_generator.class_indices.keys()))
